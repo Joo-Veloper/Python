f861d977d7813a1193438a2ff6583fd0d1dd71d6
Joo-Veloper <psjoo3515@gmail.com>
2023-11-18 23:20:44 +0900

    TestCode

--- hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,10 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
 
 import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository{
+
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
     @Override
@@ -29,5 +31,9 @@ public class MemoryMemberRepository implements MemberRepository{
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
