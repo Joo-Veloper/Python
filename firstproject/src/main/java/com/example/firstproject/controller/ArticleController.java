68bcf609198f0b7c6f717f6f7c93e1af8143f16a
Joo-Veloper <psjoo3515@gmail.com>
2023-11-20 14:14:28 +0900

    Rombok

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j //로깅 기능을 위한 어노테이션
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
@@ -21,10 +23,15 @@ public class ArticleController {
 
     @PostMapping("/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());// 로깅 코드
+//        System.out.println(form.toString());
+        //DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+//        System.out.println(article.toString());
+        // 리파지터리로 엔티티 DB에 저장
         Article saved = articleRepository.save(article);
+        log.info(saved.toString());
         System.out.println(saved.toString());
         return "";
     }
