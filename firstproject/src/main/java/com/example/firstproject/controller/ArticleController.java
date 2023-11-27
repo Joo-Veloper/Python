6b2f04456440a362c3a7ea24ad15ec7af7c5a91c
Joo-Veloper <psjoo3515@gmail.com>
2023-11-27 23:14:58 +0900

    id 조회

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,8 +38,10 @@ public class ArticleController {
         return "";
     }
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable Long id) {
+    public String show(@PathVariable Long id, Model model) {
         log.info("id = " + id);
+        //1. id 조회
+        Article articleEntity = articleRepository.findById(id).orElse(null);
 
         return "";
     }
